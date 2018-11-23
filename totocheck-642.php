81d5dd6214efa9c3f3e7fdf527ba8235706cc756
nosorrow <plamenorama@gmail.com>
2018-11-23 20:13:58 +0200

    toto 4

--- totocheck-642.php
+++ totocheck-642.php
@@ -22,7 +22,7 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="resultModalLabel">Резултат от архива 1958 - 2018 г.</h5>
+                <h5 class="modal-title" id="resultModalLabel">Резултат</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
