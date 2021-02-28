afb28885493233c3b58a300e361f445e4c0d191c
nosorrow <plamenorama@gmail.com>
2021-02-28 19:56:42 +0200

    2021-02-28 twentyone

--- Class/UpdateDraws.php
+++ Class/UpdateDraws.php
@@ -71,12 +71,9 @@ class UpdateDraws
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-        if (!empty($post)) {
-            curl_setopt($ch, CURLOPT_POST, true);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-        }
+       // curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+       // curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+
         $raw_data = curl_exec($ch);
         curl_close($ch);
 
