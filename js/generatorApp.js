0e66c94d70a212613e78eb37ded0682e10493f7b
nosorrow <plamenorama@gmail.com>
2021-03-12 22:00:59 +0200

    2021-03-12

--- js/generatorApp.js
+++ js/generatorApp.js
@@ -1,60 +1,56 @@
 async function generator(n, e) {
-    e.preventDefault();
-    $("#draw1").html("");
-    var row = $("table").find('tr');
-    row.removeClass('table-success');
-    if (n == 535) {
-        $("#six-tr-g").hide();
-    } else {
-        $("#six-tr-g").show();
+  e.preventDefault();
+  $("#draw1").html("");
+  var row = $("table").find("tr");
+  row.removeClass("table-success");
+  if (n == 535) {
+    $("#six-tr-g").hide();
+  } else {
+    $("#six-tr-g").show();
+  }
+
+  //   res.done(function (result) {
+  let response = await fetch("generator.php?igra=" + n);
+  let result = await response.json();
+
+  jQuery.each(result.kombinacia, function (i, field) {
+    $("#draw1").append('<span class="ball">' + field + "</span>");
+  });
+
+  if (result.statistics) {
+    if (result.statistics.six > 0) {
+      $("#six-tr-g").addClass("table-success");
     }
 
- //   res.done(function (result) {
-    let response  = await fetch('generator.php?igra='+n);
-    let result = await response.json();
+    if (result.statistics.five > 0) {
+      $("#five-tr-g").addClass("table-success");
+    }
 
-    jQuery.each(result.kombinacia, function (i, field) {
-            $("#draw1").append('<span class="ball">' + field + '</span>');
-        });
+    if (result.statistics.four > 0) {
+      $("#four-tr-g").addClass("table-success");
+    }
 
-        if (result.statistics) {
-            if (result.statistics.six > 0) {
-                $('#six-tr-g').addClass('table-success');
-            }
+    if (result.statistics.three > 0) {
+      $("#three-tr-g").addClass("table-success");
+    }
+    console.log(result.statistics.three);
 
-            if (result.statistics.five > 0) {
-                $('#five-tr-g').addClass('table-success');
-            }
+    $("#six-td-g").html(result.statistics.six);
+    $("#five-td-g").html(result.statistics.five);
+    $("#four-td-g").html(result.statistics.four);
+    $("#three-td-g").html(result.statistics.three).addClass("table-active");
 
-            if (result.statistics.four > 0) {
-                $('#four-tr-g').addClass('table-success');
-            }
+    $("#generatorModal").modal("show");
+  }
 
-            if (result.statistics.three > 0) {
-                $('#three-tr-g').addClass('table-success');
-            }
-            console.log(result.statistics.three);
-
-            $('#six-td-g').html(result.statistics.six);
-            $('#five-td-g').html(result.statistics.five);
-            $('#four-td-g').html(result.statistics.four);
-            $('#three-td-g').html(result.statistics.three).addClass('table-active');
-
-            $('#generatorModal').modal('show');
-
-        }
-
-   //   });
-
+  //   });
 }
-
-
-function getResult(n) {
-    return $.ajax({
-        url: "generator.php",
-        method: "POST",
-        dataType: "json",
-        async: false,
-        data: {igra: n}
-    });
-}
+//
+// function getResult(n) {
+//   return $.ajax({
+//     url: "generator.php",
+//     method: "POST",
+//     dataType: "json",
+//     data: { igra: n },
+//   });
+// }
--- totocheck-642.php
+++ totocheck-642.php
@@ -19,7 +19,7 @@ if (file_exists($filename)) {
   <link rel="stylesheet" href="css/bootstrap.css">
   <link rel="stylesheet" href="css/style.css">
   <!-- Latest compiled and minified JavaScript -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.js"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
           integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
