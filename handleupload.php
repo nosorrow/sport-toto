595113be3a4ee6d1fdcd873f1c087438b8a82910
nosorrow <plamenorama@gmail.com>
2021-03-13 18:30:16 +0200

    2021-03-13 1

--- handleupload.php
+++ handleupload.php
@@ -10,8 +10,6 @@ if ($apikey === 'apikey') {
     foreach ($_FILES["file"]["error"] as $key => $error) {
         if ($error === UPLOAD_ERR_OK) {
             $tmp_name = $_FILES["file"]["tmp_name"][$key];
-            // basename() may prevent filesystem traversal attacks;
-            // further validation/sanitation of the filename may be appropriate
             $name = basename($_FILES["file"]["name"][$key]);
             if (move_uploaded_file($tmp_name, "$uploads_dir/$name")) {
                 echo "Successfully uploaded file $name\n";
